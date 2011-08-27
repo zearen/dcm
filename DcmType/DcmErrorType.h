34a6bb2179fd19d9a665c764cb3e6415e322f948
Zearen Wover <zearen.wover@hotmail.com>
2011-08-27 13:29:55 -0400

    Finished DcmError, about test

--- DcmType/DcmErrorType.h
+++ DcmType/DcmErrorType.h
@@ -11,59 +11,51 @@
 #define CUSTOM_E        8
 
 class DcmError : public DcmType {
-    protected:
-        static const string msg;
     public:
-        DcmError();
-        DcmError(DcmError& toCopy);
         TypeVal type();
         string repr();
 };
 
 class DcmTypeError : public DcmError {
     protected:
-        static const string msg;
         TypeVal exp, got;
     public:
         DcmTypeError(DcmTypeError& toCopy);
         DcmTypeError(TypeVal expected, TypeVal received);
-        TypeVal type();
-        string repr();
         TypeVal expected();
         TypeVal received();
+        TypeVal type();
+        string repr();
 };
 
 class DcmBoundsError : public DcmError {
     protected:
-        static const string msg;
         int top, giv;
     public:
         DcmBoundsError(DcmBoundsError& toCopy);
         DcmBoundsError(int topBound, int given);
-        TypeVal type();
-        string repr();
         int topBound();
         int given();
+        TypeVal type();
+        string repr();
 };
 
 class DcmStackError : public DcmError {
     protected:
-        static const string msg;
         DcmSymbol dcmStack;
         TypeVal sender;
     public:
         DcmStackError(DcmStackError& toCopy);
         DcmStackError(DcmSymbol sym, TypeVal sentFrom);
-        TypeVal type();
-        string repr();
         TypeVal senderType();
         DcmSymbol getSymbol();
+        TypeVal type();
+        string repr();
 }; 
 
 // Inherit this for custom errors
 class DcmCustomError : public DcmError {
     protected:
-        static const string msg;
         string customMsg;
     public:
         DcmCustomError(DcmCustomError& toCopy);
