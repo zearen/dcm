cd7f47618b3b6d5cc80d63b4ec3548f71d4c6781
Zearen Wover <zearen.wover@gmail.com>
2012-02-24 17:15:54 -0500

    Plugins weren't changed

--- plugins/io.cpp
+++ plugins/io.cpp
@@ -1,31 +1,44 @@
 #include "io.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+static ostream& dcmout = cout;
+
+static istream& dcmin  = cin;
+
 void cbPrint(DcmStack& stk) {
-    cout << safePeekMain(stk)->repr() << endl;
+    DcmType *dcm = safePeekMain(stk);
+    stk.pop();
+    dcmout << dcm->repr() << endl;
+    del(dcm);
 }
 
-void cbF(DcmStack& stk) {
+void cbPStk(DcmStack& stk) {
     stack<DcmType*> items;
     while (!stk.empty()) {
         items.push(stk.top());
         stk.pop();
     }
     while (!items.empty()) {
-        cout << items.top()->repr() << ' ';
+        dcmout << items.top()->repr() << ' ';
         stk.push(items.top());
         items.pop();
     }
-    cout << endl;
+    dcmout << endl;
 }
 
+void cbInput(DcmStack& stk) {
+    string str;
+    getline(dcmin, str);
+    stk.push(new DcmString(str));
+}
 
 Plugin * ioPlugin() {
-    vector<Callback*> v =
-        { addName(new SimpleCallback(cbPrint), "print")
-        , addName(new SimpleCallback(cbF), "f")
+    vector<NamedCB> v =
+        { NamedCB("print",  new SimpleCallback(cbPrint))
+        , NamedCB("p-stk",  new SimpleCallback(cbPStk))
         };
     return new VectorPlugin(v);
 }
--- plugins/prelude.cpp
+++ plugins/prelude.cpp
@@ -43,13 +43,46 @@ void cbRefs(DcmStack& stk) {
     stk.push(new DcmInt(dcm->refCount()));
 }
 
+void cbDef(DcmStack& stk) {
+    DcmExec *exec = static_cast<DcmExec*>(safePeekMain(stk,
+      DcmExec::typeVal()));
+    stk.pop();
+    stk.push(new DcmPrimFun(new ExecCallback(exec)));
+    // We took it off the stack
+    del(exec);
+}
+
+// Eventually we'll want ExecStrCallback("$a $b @a @b");
+void cbR(DcmStack& stk) {
+    DcmType **dcms = popN(stk, 2);
+    stk.push(dcms[0]);
+    stk.push(dcms[1]);
+    delete dcms;
+}
+
+void cbIs(DcmStack& stk) {
+    DcmType **dcms = popN(stk, 2);
+    stk.push(new DcmBool(dcms[0] == dcms[1]));
+    del(dcms[0]); del(dcms[1]);
+    delete dcms;
+}
+
+void cbCopy(DcmStack& stk) {
+    DcmType *dcm = safePeekMain(stk);
+    stk.push(dcm->copy());
+}
+
 Plugin *preludePlugin() {
-    vector<Callback*> v = 
-        { addName(new SimpleCallback(cbDup), "dup")
-        , addName(new SimpleCallback(cbDel), "del")
-        , addName(new SimpleCallback(cbClear), "clear")
-        , addName(&cbX, "x")
-        , addName(new SimpleCallback(cbRefs), "refs")
+    vector<NamedCB> v = 
+        { NamedCB("dup",    new SimpleCallback(cbDup))
+        , NamedCB("del",    new SimpleCallback(cbDel))
+        , NamedCB("clear",  new SimpleCallback(cbClear))
+        , NamedCB("x",      &cbX)
+        , NamedCB("refs",   new SimpleCallback(cbRefs))
+        , NamedCB("r",      new SimpleCallback(cbR))
+        , NamedCB("def",    new SimpleCallback(cbDef))
+        , NamedCB("is",     new SimpleCallback(cbIs))
+        , NamedCB("copy",   new SimpleCallback(cbCopy))
         };
     return new VectorPlugin(v);
 }
